c349c2b24a1044984806ff37fcc0c54cfc57023c
AyushR97j <ayushraj803110@gmail.com>
2024-11-20 18:05:10 +0530

    query segment function

--- Segment Tree/segmentTree.cpp
+++ Segment Tree/segmentTree.cpp
@@ -11,5 +11,18 @@ void buildSegmentTree(int i, int l, int r, vector<int> &segmentTree, int arr[]) 
     segmentTree[i] = segmentTree[2 * i + 1] + segmentTree[2 * i + 2];
 }
 
+int querySegmentTree(int start, int end, int i, int l, int r, vector<int>& segmentTree) {
+        if(l > end || r < start) {
+            return 0;
+        }
+        
+        if(l >= start && r <= end) {
+            return segmentTree[i];
+        }
+        
+        int mid = l + (r-l)/2;
+        return querySegmentTree(start, end, 2*i+1, l,    mid, segmentTree) + 
+               querySegmentTree(start, end, 2*i+2, mid+1, r, segmentTree);
+    }
 // T.C : O(q*log(n))
 // S.C : O(4*n)
