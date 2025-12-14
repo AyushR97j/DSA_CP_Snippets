d97eae23db60d700517bc5e88edcefc0f5daff54
Ayush Raj <ayushraj803110@gmail.com>
2025-12-14 10:27:28 +0530

    Point Update & Range Min/Sum
    Added complete implementation for Range Sum, Range Min, and Point Update using Segment Tree.

--- Segment Tree/segmentTree.cpp
+++ Segment Tree/segmentTree.cpp
@@ -27,3 +27,91 @@ int querySegmentTree(int start, int end, int i, int l, int r, vector<int>& segme
     }
 // T.C : O(q*log(n))
 // S.C : O(4*n)
+
+
+//Complete code for Range Sum & Range Min & Point Update
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    long long sum;
+    int mn;
+};
+
+vector<Node> seg;
+vector<int> arr;
+
+Node merge(Node a, Node b) {
+    return {a.sum + b.sum, min(a.mn, b.mn)};
+}
+
+void build(int i, int l, int r) {
+    if (l == r) {
+        seg[i] = {arr[l], arr[l]};
+        return;
+    }
+    int mid = (l + r) / 2;
+    build(2*i+1, l, mid);
+    build(2*i+2, mid+1, r);
+    seg[i] = merge(seg[2*i+1], seg[2*i+2]);
+}
+
+void pointUpdate(int i, int l, int r, int idx, int val) {
+    if (l == r) {
+        seg[i] = {val, val};
+        return;
+    }
+    int mid = (l + r) / 2;
+    if (idx <= mid)
+        pointUpdate(2*i+1, l, mid, idx, val);
+    else
+        pointUpdate(2*i+2, mid+1, r, idx, val);
+    seg[i] = merge(seg[2*i+1], seg[2*i+2]);
+}
+
+Node query(int i, int l, int r, int ql, int qr) {
+    if (qr < l || r < ql)
+        return {0, INT_MAX};
+    if (ql <= l && r <= qr)
+        return seg[i];
+    int mid = (l + r) / 2;
+    return merge(
+        query(2*i+1, l, mid, ql, qr),
+        query(2*i+2, mid+1, r, ql, qr)
+    );
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, q;
+    cin >> n >> q;
+
+    arr.resize(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    seg.resize(4*n);
+    build(0, 0, n-1);
+
+    while (q--) {
+        int type;
+        cin >> type;
+        if (type == 1) {
+            int idx, val;
+            cin >> idx >> val;
+            pointUpdate(0, 0, n-1, idx, val);
+        } else if (type == 2) {
+            int l, r;
+            cin >> l >> r;
+            cout << query(0, 0, n-1, l, r).mn << '\n';
+        } else if (type == 3) {
+            int l, r;
+            cin >> l >> r;
+            cout << query(0, 0, n-1, l, r).sum << '\n';
+        }
+    }
+}
+
+*/
