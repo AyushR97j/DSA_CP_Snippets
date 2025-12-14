893e76b385854977acf958b590369eb09a9fc83f
Ayush Raj <ayushraj803110@gmail.com>
2025-12-14 08:09:20 +0530

    Added some clarifications/comments

--- Segment Tree/segmentTree.cpp
+++ Segment Tree/segmentTree.cpp
@@ -8,7 +8,8 @@ void buildSegmentTree(int i, int l, int r, vector<int> &segmentTree, int arr[]) 
     int mid = l + (r - l) / 2;
     buildSegmentTree(2 * i + 1, l, mid, segmentTree, arr);
     buildSegmentTree(2 * i + 2, mid + 1, r, segmentTree, arr);
-    segmentTree[i] = segmentTree[2 * i + 1] + segmentTree[2 * i + 2];
+    segmentTree[i] = segmentTree[2 * i + 1] + segmentTree[2 * i + 2]; //using 0-indexed array tree → children of i are 2*i+1 and 2*i+2.
+                                                                      //if using 1-indexed array tree → children of i are 2*i and 2*i+1.
 }
 
 int querySegmentTree(int start, int end, int i, int l, int r, vector<int>& segmentTree) {
